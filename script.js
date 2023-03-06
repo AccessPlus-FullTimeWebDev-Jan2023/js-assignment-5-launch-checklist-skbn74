a121ab84117c7ad514b01ccc8651860e4b3441b0
Sergio <buchanansergio@gmail.com>
2023-03-06 10:34:17 -0600

    updates to addDestinationInfo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
     
@@ -14,7 +14,8 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-      let destination =  pickPlanet(listedPlanetsResponse());
+      formSubmission();
+       let destination =  pickPlanet(listedPlanetsResponse());
         return addDestinationInfo(destination);
    });
    
